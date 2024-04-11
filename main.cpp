26b6533d2c764f3cf8c5d98dc66e8360e06c65c7
avros16 <114757828+avros16@users.noreply.github.com>
2024-04-11 12:37:58 +0000

    Q2, comments and final

--- main.cpp
+++ main.cpp
@@ -3,19 +3,17 @@ using namespace std;
 
 // Q2 Write a function that takes the time as three integer arguments(hours, minutes and seconds) and returns the number of seconds since the last time the clock "stuck 12". Use this function to calculate the amount of time in seconds between two times, both of which are within one 12-hour cycle of the clock. Eg: if user enters 3,12,39, the output is 11559.
 
+//function will do the math
+int time(int hour, int mins, int sec){ //taking in  hour, mins and secs
+int seconds {0}; //will hold seconds
 
-int time(int hour, int mins, int sec){
-int seconds {0}; 
-
-seconds = hour*3600;
-seconds = seconds + (mins*60);
-seconds = seconds + sec;
+seconds = hour*3600; //math for hour. 3,600 seconds in one hour
+seconds = seconds + (mins*60);//math for mins
+seconds = seconds + sec; //math for seconds
   
 
   return seconds;
 }
-
-
 
 
 
@@ -24,16 +22,18 @@ int main() {
   int hours{0};
   int mins{0};
   int sec{0};
-  int grab{0};
+  int grab{0}; //grab and store value from function
 
-  cout << "What time is it? (Enter hour, not in military time): ";
+  // get values from user
+  
+  cout << "What time is it? (Enter hour, not in military time): "; //not in military time ensures the "since 12" requirement
   cin >> hours;
   cout << "What time is it? (Enter minutes): ";
   cin >> mins;
   cout << "What time is it? (Enter seconds): ";
   cin >> sec;
 
-grab = time(hours, mins, sec);
+grab = time(hours, mins, sec); //calling function, passing in parameters, storing value in grab
   
   cout << "The total time that has passed since the clock hit 12 is: " << grab << " seconds"; 
   
